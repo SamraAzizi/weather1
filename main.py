4b05cdd5dcadf01a1f0a4eab9e78944143743a15
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-12 13:50:37 +0500

    my commit

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ from geopy.geocoders import Nominatim
 from tkinter import ttk, messagebox
 from timezonefinder import TimezonrFinder
 from datetime import datetime
+import requests
+import pytz
+
+root = Tk()
+root.title("Weather App")
+root.geometry("900x500+300+200")
+root.resizable()
