13376d35d2b974fbf813a249428d9189a75b8941
SamraAzizi <fahimaazizi369@gmail.com>
2024-07-14 16:44:19 +0500

    my commit

--- main.py
+++ main.py
@@ -12,6 +12,14 @@ root.title("Weather App")
 root.geometry("900x500+300+200")
 root.resizable(False, False)
 
+def getWeather():
+    city= textField.get()
+
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    location = geolocator.geocode(city)
+    obj = TimezonrFinder()
+
+
 
 #search box
 
@@ -26,7 +34,7 @@ textField.focus()
 
 
 Search_icon = PhotoImage(file="search_icon.png")
-myimage_icon = Button(image=Search_icon, borderwidth=0, cursor="hand2", bg="#404040")
+myimage_icon = Button(image=Search_icon, borderwidth=0, cursor="hand2", bg="#404040", command=getWeather)
 myimage_icon.place(x=400, y=34)
 
 
