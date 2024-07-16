b3717a3f6c502aca60a72117e6ff2cdf65f81fb7
SamraAzizi <samarraazizi@gmail.com>
2024-07-16 13:34:47 +0500

    my commit

--- main.py
+++ main.py
@@ -27,10 +27,7 @@ def getWeather():
         if result is None:
             raise ValueError("Could not find the timezone")
 
-        home = pytz.timezone(result)
-        local_time = datetime.now(home)
-        current_time = local_time.strftime("%I:%M %p")
-        clock.config(text=current_time)
+        home = pytz.time
         name.config(text="CURRENT WEATHER")
 
         # weather
